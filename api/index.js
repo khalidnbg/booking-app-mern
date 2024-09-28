403cf5d0d04abb584c05468493c23f0dda630a20
khalidnbg <khalidnabgaoui4@gmail.com>
2024-09-28 22:13:44 +0100

    get single place logic

--- api/index.js
+++ api/index.js
@@ -167,7 +167,7 @@ app.post("/places", (req, res) => {
       owner: userData.id,
       title,
       address,
-      addedPhotos,
+      photos: addedPhotos,
       description,
       perks,
       extraInfo,
@@ -177,6 +177,21 @@ app.post("/places", (req, res) => {
     });
     res.json(placeDoc);
   });
+});
+
+app.get("/places", (req, res) => {
+  const { token } = req.cookies;
+
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await PlaceModel.find({ owner: id }));
+  });
+});
+
+app.get("/places/:id", async (req, res) => {
+  const { id } = req.params;
+
+  res.json(await PlaceModel.findById(id));
 });
 
 app.listen(4000);
